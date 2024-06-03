b82f0324c0c726894d20055ecf925724c11802e7
Pratham K S <prathamks2302@gmail.com>
2024-06-03 16:05:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/Auth";
+import { login, logout } from "./store/AuthSlice";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import { Outlet } from "react-router-dom";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, [loading]);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Logo from "./Logo";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import LogoutBtn from "./header/LogoutBtn";
+import Container from "./container/Container";
+import Button from "./Button";
+import Select from "./Select";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
+import Protected from "./AuthLayout";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
+
+export {
+  Header,
+  Footer,
+  Logo,
+  LogoutBtn,
+  Container,
+  Button,
+  Select,
+  Input,
+  PostCard,
+  Login,
+  Signup,
+  Protected,
+  RTE,
+  PostForm,
+};
